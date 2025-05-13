547f43e7caa6061e6565f981d8ac06e9310bec48
daqiq25 <sebastien.daqiq25@gmail.com>
2025-05-13 13:17:45 +0200

    2-sort.rb

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,31 +1,28 @@
-###
+
+#  Sort integer arguments (ascending) 
 #
-#  Sort integer arguments (ascending)
-#
-###
+
 
 result = []
+
 ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
-    # convert to integer
-    i_arg = arg.to_i
-    # insert result at the right position
-    result << i_arg
-end
+  # skip if not integer
+  next if arg !~ /^-?\d+$/
 
-n = result.length
-loop do
-    swapped = false
+  # convert to integer
+  i_arg = arg.to_i
 
-    (n-1).times do |i|
-        if result[i] < result[i+1]
-            result[i], result[i+1] = result[i+1], result[i]
-            swapped = true
-        end
+  # find correct position to insert
+  inserted = false
+  result.each_with_index do |val, index|
+    if i_arg < val
+      result.insert(index, i_arg)
+      inserted = true
+      break
     end
+  end
 
-    break if not swapped
+  result << i_arg unless inserted
 end
 
 puts result
